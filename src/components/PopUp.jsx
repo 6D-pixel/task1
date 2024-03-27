4b966d55ec418e15fe46c07fb10f6c7efd7fa744
6D-pixel <dinesh7rd@gmail.com>
2024-03-27 15:49:20 +0530

    small changes

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './PopUp.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./PopUp.css";
 const PopUp = ({ country, closeModal }) => {
   const [weatherInfo, setWeatherInfo] = useState(null);
   const lat = country.capitalInfo.latlng[0];
@@ -9,22 +9,28 @@ const PopUp = ({ country, closeModal }) => {
   useEffect(() => {
     const fetchWeatherInfo = async () => {
       try {
-        const response_key = await axios.get(`https://dataservice.accuweather.com/locations/v1/cities/geoposition/search`, {
-          params: {
-            apikey:`${import.meta.env.VITE_API_KEY}`,//api key
-            q:`${lat},${lng}`
+        const response_key = await axios.get(
+          `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search`,
+          {
+            params: {
+              apikey: import.meta.env.VITE_API_KEY, //api key
+              q: `${lat},${lng}`,
+            },
           }
-        });
+        );
         const weatherKey = response_key.data.Key;
-        const response_weather = await axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${weatherKey}`,{
-            params:{
-                apikey:`${import.meta.env.VITE_API_KEY}`
-            }
-        })
+        const response_weather = await axios.get(
+          `https://dataservice.accuweather.com/currentconditions/v1/${weatherKey}`,
+          {
+            params: {
+              apikey: import.meta.env.VITE_API_KEY,
+            },
+          }
+        );
         setWeatherInfo(response_weather.data);
-        console.log(response_weather.data)
+        console.log(response_weather.data);
       } catch (error) {
-        console.error('Error fetching weather info:', error);
+        console.error("Error fetching weather info:", error);
       }
     };
     fetchWeatherInfo();
@@ -34,7 +40,9 @@ const PopUp = ({ country, closeModal }) => {
     <div className="modal-container">
       <div className="modal">
         <div className="modal-content">
-          <span className="close" onClick={closeModal}>&times;</span>
+          <span className="close" onClick={closeModal}>
+            &times;
+          </span>
           <h2>{country.name.common}</h2>
           <p>Population: {country.population}</p>
           <p>Continent: {country.region}</p>
